b26219089428b43cc6df912aabf8bef95dafe8ee
Tapas Biswas <tapasbiswas1999@gmail.com>
2022-01-26 12:20:24 +0530

    Initial Commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+
+    return (
+        <footer className="bd-footer py-5 mt-5 bg-light" >
+            <p className='text-center'>Copyright &copy; 2020–2021 MyTodosList.com</p>
+        </footer>
+    )
+}
+
